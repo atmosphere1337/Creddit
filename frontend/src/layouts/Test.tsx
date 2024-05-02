7c36f860c48ecffec4d22c038095fe65c8821bb7
Igor Boldyrev <agbdlcider@gmail.com>
2024-05-02 18:36:25 +0600

    2.4.1 Install and configure all modules for React

---
+++ frontend/src/layouts/Test.tsx
@@ -0,0 +1,40 @@
+// hook check
+import React, {useState} from 'react';
+// material ui check
+import Button from '@mui/material/Button';
+// redux check
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../other/counterSlice'
+import { useAppSelector, useAppDispatch } from '../other/hooks'
+import styled from "styled-components"
+
+
+
+function Test() {
+    const count = useAppSelector((state) => state.counter.value)
+    const dispatch = useAppDispatch()
+    const [value, setValue] = useState("Hello world")
+    return (
+        <>
+            <Styled>
+                I'm styled component
+            </Styled>
+            <div>
+                { value }
+            </div>
+            <div>
+                { count }
+            </div>
+            <div>
+                <Button variant="contained" onClick={ () => dispatch(decrement()) }>Hello Orwell</Button>
+            </div>
+            <div onClick={ () => { dispatch(increment()) } } >
+                hello Orwell
+            </div>
+        </>
+    )
+}
+const Styled = styled.div`
+    background-color: blue;
+`
+export default Test;
