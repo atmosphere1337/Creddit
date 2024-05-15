7a2cc8a5e392d8ae64a4b14a722b21bdfde486b7
Igor Boldyrev <agbdlcider@gmail.com>
2024-05-15 11:42:10 +0700

    2.5.10 | /CommentCard.tsx | Add avatar circle

--- frontend/src/large-components/CommentCard.tsx
+++ frontend/src/large-components/CommentCard.tsx
@@ -5,26 +5,33 @@ import ReplyButton from "../small-components/ReplyButton/ReplyButton";
 import GeneralButton from "../small-components/GeneralButton";
 type TypeProps =  { name:string, comment:string, rating: number, age:number };
 function CommentCard({ name = "default", comment = "default", rating = 0, age = 0 } : TypeProps) {
-    const [randomColor, setRandomColor] = useState(Math.floor(100 * Math.random()));
     const colors : string[] = ["red", "blue", "yellow", "green", "gray", "blueviolet", "brown", "aquamarine"];
+    const [randomColor, setRandomColor] = useState(Math.floor(100 * Math.random()) % colors.length);
     return (
         <StyledDiv>
-            <div style={{display: "flex"}}>
-                <div style={{marginRight: "20px"}}>
-                    {name}
+            <div style={{ display: "flex" }}>
+                <div style={{ marginRight: "20px" }}>
+                    <StyledAvatarDiv dynamicColor={ colors[randomColor] } />
                 </div>
                 <div>
-                    {age}
+                    <div style={{display: "flex", marginBottom: "10px"}}>
+                        <div style={{marginRight: "20px", fontWeight: "bold"}}>
+                            {name}
+                        </div>
+                        <div style={{ color: "#9f9e9e" }}>
+                            {age} minutes ago
+                        </div>
+                    </div>
+                    <div>
+                        {comment}
+                    </div>
+                    <StyledButtonPadDiv>
+                        <RatingButton value={rating}/>
+                        <ReplyButton/>
+                        <GeneralButton value={"Report"} link={"suckmydickd"} color={"red"}/>
+                    </StyledButtonPadDiv>
                 </div>
             </div>
-            <div>
-                {comment}
-            </div>
-            <StyledButtonPadDiv>
-                <RatingButton value={ rating } />
-                <ReplyButton />
-                <GeneralButton value={ "Report" } link={"suckmydickd"} color={"red"} />
-            </StyledButtonPadDiv>
         </StyledDiv>
     );
 }
@@ -33,10 +40,10 @@ const StyledDiv = styled.div`
     margin-bottom: 20px;
     padding: 20px;
     border-radius: 15px;
-    background-color: gray;
+    background-color: #403f3f;
 `;
-const StyledAvatarDiv = styled.div`
-    background-color: blue;
+const StyledAvatarDiv = styled.div<{ dynamicColor?: string }>`
+    background-color: ${ props => props.dynamicColor };
     height: 30px;
     width: 30px;
     border-radius: 666px;
@@ -47,5 +54,9 @@ const StyledButtonPadDiv = styled.div`
     gap: 7px;
     margin-top: 15px;
 `;
+const StyledCommentDiv = styled.div`
+    
+`;
+
 
 export default CommentCard;
