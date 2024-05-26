c3b6dae328b58fb943237acec1d971e889776bd3
atmosphere1337 <agbdlcider@gmail.com>
2024-05-26 11:24:49 +0800

    2.6.3 | /userSlice.ts | Init states from cookies

--- frontend/src/other/userSlice.ts
+++ frontend/src/other/userSlice.ts
@@ -6,9 +6,46 @@ interface IUserState  {
     isLoggedIn: boolean,
 }
 
+//-----------------------------------------------------------------------------------------
+const LOGGEDIN_COOKIE_NAME : string = "credditLoggedIn";
+const COLORTHEME_COOKIE_NAME : string = "credditColorTheme";
+
+function getCookie(cookieName : string) : string | undefined {
+    const cookie : string | undefined = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith(cookieName))
+        ?.split("=")[1];
+    return cookie;
+}
+
+function setCookie(cookieName : string, cookieValue : string) : void {
+    document.cookie = `${cookieName}=${cookieValue}; max-age=${60*60*24*365}; path=/; SameSite=Lax;`;
+}
+
+function doWeRenderGuestOrUserLayout() : boolean {
+    let loggedInCookieValue : string | undefined = getCookie(LOGGEDIN_COOKIE_NAME);
+    switch (loggedInCookieValue) {
+        case "true":
+            return true;
+        case "false":
+            return false;
+        default:
+            setCookie(LOGGEDIN_COOKIE_NAME, "false");
+            return false;
+    }
+}
+function doWeRenderLightOrDarkMode() : "light" | "dark" {
+    let colorThemeCookieValue : string | undefined = getCookie(COLORTHEME_COOKIE_NAME);
+    if (colorThemeCookieValue === "light" || colorThemeCookieValue === "dark")
+        return colorThemeCookieValue;
+    setCookie(COLORTHEME_COOKIE_NAME, "dark");
+    return "dark";
+}
+
+//-----------------------------------------------------------------------------------------
 const initialState : IUserState = {
-    colorMode: "dark",
-    isLoggedIn: false,
+    colorMode: doWeRenderLightOrDarkMode(),
+    isLoggedIn: doWeRenderGuestOrUserLayout(),
 }
 export const userSlice = createSlice({
     name: "user",
