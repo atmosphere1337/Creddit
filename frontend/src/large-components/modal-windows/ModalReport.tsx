7ed682670edd64247ee83d8a7b94e4ed5b8806d5
atmosphere1337 <agbdlcider@gmail.com>
2024-06-03 04:07:59 +0700

    2.6.8 | ModalReport.tsx | Create component

---
+++ frontend/src/large-components/modal-windows/ModalReport.tsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, Typography } from '@mui/material';
+import GeneralButton from '../../small-components/GeneralButton';
+function ModalReport() {
+    const [show, setShow] = useState<boolean>(false);
+    return (
+        <>
+            <div onClick={() => setShow(true)} style={{cursor: "pointer"}}>
+                <GeneralButton value={"Report"} link={"suckmydickd"} color={"red"} />
+            </div>
+            <Dialog open={show} onClose={() => setShow(false)}>
+                <DialogTitle>
+                    Report window
+                </DialogTitle>
+                <DialogContent>
+                    <Typography component="div">
+                        Enter the description of violation
+                    </Typography>
+                    <TextField>
+                        chel
+                    </TextField>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="contained" color="error">
+                        Submit
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    )
+            
+}
+export default ModalReport;
