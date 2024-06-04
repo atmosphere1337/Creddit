e6d63defe3ba5145afd385f954be431756d87bed
Igor Boldyrev <agbdlcider@gmail.com>
2024-06-04 15:31:32 +0700

    2.6.24 | /store.ts | Add user, channel, ads, posts, reports reducers to storage

--- frontend/src/other/store.ts
+++ frontend/src/other/store.ts
@@ -1,11 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit'
-import commentReducer from './commentSlice';
-import userReducer from './userSlice';
+import commentReducer from './slices/commentSlice';
+import userReducer from './slices/userSlice';
+import postReducer from "./slices/postSlice";
+import advertisementReducer from  "./slices/advertisementSlice";
+import reportReducer from "./slices/reportSlice";
+import popularChannelReducer from "./slices/popularChannelsSlice";
+import channelInfoReducer from './slices/channelInfoSlice';
+import userDataReducer from "./slices/userDataSlice";
 
 export const store = configureStore({
     reducer: {
         comment: commentReducer,
         user: userReducer,
+        post: postReducer,
+        ads: advertisementReducer,
+        report: reportReducer,
+        popularChannel: popularChannelReducer,
+        channelInfo: channelInfoReducer,
+        userData : userDataReducer,
     },
 })
 export type RootState = ReturnType<typeof store.getState>
