6f24161ce6debc1cc5e2cf77facec22c1776254b
Igor Boldyrev <agbdlcider@gmail.com>
2024-06-21 12:09:47 +0700

    2.7.42 | /PostSmall.tsx | Create post deletion apicall and button

--- frontend/src/large-components/PostSmall/PostSmall.tsx
+++ frontend/src/large-components/PostSmall/PostSmall.tsx
@@ -1,13 +1,33 @@
-import React, { useState } from "react";
+import React, {FC, JSX, useState} from "react";
 import styled from "styled-components";
 import ModalReport from "large-components/modal-windows/ModalReport";
 import RatingButton from "small-components/RatingButton/RatingButton";
 import CommentsButton from "small-components/CommentsButton/CommentsButton";
 import { StyledA } from "other/styles/CommonStyles";
 import {IPostMini} from "../../other/widelyUsedTypes";
+import {IconButton} from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import axios, {AxiosResponse} from "axios";
+import {useParams} from "react-router-dom";
 
-function PostSmall({props} : {props: IPostMini}) {
-    function parseBody() {
+function PostSmall({props} : {props: IPostMini}) : JSX.Element {
+    const [isHidden, setIsHidden] = useState<boolean>(false);
+    const params = useParams();
+    function deletePostHandler(): void {
+        axios.delete(`/api/post/${props.id}`)
+            .then(
+                (response : AxiosResponse<any>) : void => {
+                    setIsHidden(true);
+                }
+            )
+            .catch(
+                error => {
+                    alert('during delete post apicall an error has occurred');
+                }
+            );
+    }
+
+    function parseBody() : JSX.Element {
         const slices : string[] = props.body.split("***");
         return (
             <>
@@ -26,21 +46,26 @@ function PostSmall({props} : {props: IPostMini}) {
             </>
         );
     }
-    return (
+
+    const PostComponentToBeRendered : FC = () => (
         <Styleddiv>
-          <div>
-            <StyledA href={ `/c/${props.channelId}/posts/${props.id}`}>
-              {props.name}
-            </StyledA>
-          </div>
+            <div style={{display: "flex"}}>
+                <StyledA href={ `/c/${props.channelId}/posts/${props.id}`}>
+                    {props.name}
+                </StyledA>
+                <IconButton sx={{marginLeft: "auto"}} onClick={deletePostHandler}>
+                    <DeleteIcon fontSize="small"/>
+                </IconButton>
+            </div>
             { parseBody() }
-          <StyledOptions>
-            <RatingButton value={props.rating}/>
-            <CommentsButton value={props.comments}></CommentsButton>
-            <ModalReport />
-          </StyledOptions>
+            <StyledOptions>
+                <RatingButton value={props.rating}/>
+                <CommentsButton value={props.comments}></CommentsButton>
+                <ModalReport />
+            </StyledOptions>
         </Styleddiv>
-    );
+    )
+    return isHidden? <></> : <PostComponentToBeRendered />;
 }
 
 const Styleddiv = styled.div`
