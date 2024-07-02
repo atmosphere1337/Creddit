03903c73ee3ccce79486a0f78f4694c6003c1bb6
Igor Boldyrev <agbdlcider@gmail.com>
2024-07-02 19:36:09 +0700

    2.8.22 | /UserSettingsPage.tsx | Fetch User data on usersettings page via apicall

--- frontend/src/pages/UserSettingsPage.tsx
+++ frontend/src/pages/UserSettingsPage.tsx
@@ -1,15 +1,49 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Box, Grid, Container, TextField, Button, Typography, Alert, Collapse, IconButton, Divider } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
+import axios from "axios";
+import {getCookie} from "../other/widelyUsedFunctions";
+
+interface IUserSettingsData {
+    email : string,
+    username: string,
+    profilePictureUrl: string,
+}
+const userSettingsInitData : IUserSettingsData = {
+    email: "",
+    username: "",
+    profilePictureUrl: "default",
+}
 
 function UserSettingsPage() {
     const [oldPassword, setOldPassword] = useState<string>("");
     const [newPassword, setNewPassword] = useState<string>("");
     const [openCollapseAlert,setOpenCollapseAlert] = useState<boolean>(false);
+    const [userSettingsData, setUserSettingsData] = useState<IUserSettingsData>(userSettingsInitData);
     function handlePasswordChange(){
         setOpenCollapseAlert(true);
-        //apicall
+
     }
+    useEffect((): void => {
+        const config : {headers: {"Authorization" : string}} = {
+            headers: {
+                "Authorization" : `Bearer ${getCookie("token")}`,
+            }
+        }
+        axios.get('/api/user', config)
+            .then((response): void => {
+                const fetchedSettingsData: IUserSettingsData = {
+                    email : response.data.email,
+                    username: response.data.username,
+                    profilePictureUrl: response.data.profilePictureUrl,
+                };
+                setUserSettingsData(fetchedSettingsData);
+            })
+            .catch((): void => {
+                alert('Getting user info an error has occurred');
+            });
+    }, []);
+
     return (
         <Container maxWidth="md" >
             <Box sx={{height: "90vh"}}>
@@ -21,11 +55,32 @@ function UserSettingsPage() {
                         Avatar
                     </Grid> 
                     <Grid item xs={8}>
-                        <Button variant="contained" component="label">
-                            <input type="file" hidden></input>
-                            Upload image                            
-                        </Button>
-                    </Grid> 
+                        <Box>
+                            <Button variant="contained" component="label">
+                                <input type="file" hidden></input>
+                                Upload image
+                            </Button>
+                        </Box>
+                        <Box>
+                            <TextField sx={{mt: 2}}
+                                fullWidth
+                                value={userSettingsData.profilePictureUrl}
+                                onChange={(e): void => {
+                                    setUserSettingsData( {...userSettingsData, profilePictureUrl: e.target.value})
+                                }}
+                            />
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12}><Divider /></Grid>
+                    <Grid item xs={4}>
+                        Username
+                    </Grid>
+                    <Grid item xs={8}>
+                        <TextField
+                            value={userSettingsData.username}
+                            onChange={e =>{setUserSettingsData({...userSettingsData, username: e.target.value})}}
+                        />
+                    </Grid>
                     <Grid item xs={12}><Divider /></Grid>
                     <Grid item xs={4}>
                         Change password
