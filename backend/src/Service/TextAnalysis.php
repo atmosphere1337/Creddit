32fb292dc272bda2db2795a5b03c5662b91367a6
Igor Boldyrev <agbdlcider@gmail.com>
2024-07-08 11:28:37 +0700

    2.8.58 | /TextAnalysis.php | Text analysis rules are objects now

--- backend/src/Service/TextAnalysis.php
+++ backend/src/Service/TextAnalysis.php
@@ -1,7 +1,22 @@
 <?php
 namespace App\Service;
 
+class ProtocolRuleAction {
+    public string $criteria;
+    public int $degree;
+}
+class ProtocolRule {
+    public string $pattern;
+    public ProtocolRuleAction $action;
+    public function __construct(string $newPattern, string $newCriteria, int $newDegree) {
+        $this->pattern = $newPattern;
+        $this->action = new ProtocolRuleAction();
+        $this->action->criteria = $newCriteria;
+        $this->action->degree = $newDegree;
+    }
+}
 class TextAnalysis {
+
     private array $buzzWords = [
         [
             'pattern' => 'leftist',
@@ -10,11 +25,30 @@ class TextAnalysis {
                 'degree' => 7,
             ]
         ],
+        [
             'pattern' => 'nazi',
             'action' => [
                 'criteria' => 'left-right',
                 'degree' => 1,
-            ]
+            ],
+        ],
+        [
+            'pattern' => 'liberals',
+            'action' => [
+                'criteria' => 'lib-auth',
+                'degree' => 7,
+            ],
+        ],
+        [
+            'pattern' => 'freedom',
+            'action' => [
+                'criteria' => 'lib-auth',
+                'degree' => 2,
+            ],
+        ],
+        //new ProtocolRule('leftist', 'left-right', 7),
+
+
     ];
     public function basic($text) {
         foreach ($this->buzzWords as $buzzWord) {
